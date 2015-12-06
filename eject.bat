a3c2663ba11cc95b65adb9e6b1cd6a79ccf3ca1c
Spl3en <spl3en.contact@gmail.com>
2015-12-06 18:21:27 +0100

    JMP = PVP !

---
+++ eject.bat
@@ -0,0 +1,1 @@
+dllinject.exe --lib "%CD%\ProvokeEverybody.dll" --procname "Client_tos.exe" --dbgpriv --eject
---
+++ inject.bat
@@ -0,0 +1,2 @@
+WaitProcess.exe Client_tos.exe
+dllinject.exe --lib "%CD%\ProvokeEverybody.dll" --procname "Client_tos.exe" --dbgpriv
